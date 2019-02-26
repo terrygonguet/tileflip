7ec1c437e184a492078ebc427405384c2d540c86
terrygonguet <terry@gonguet.com>
2019-02-26 21:56:38 +0100

    Level fixes

--- src/levels/tuto-3.ts
+++ src/levels/tuto-3.ts
@@ -6,6 +6,10 @@ export default function(manager: LevelManager) {
     .insertTile("TileCircle", undefined, false, "up")
     .insertToast("Anywhere but left!", 2)
     .insertTile("TileCircle", undefined, false, "left")
+    .insertToast("Anywhere but right!", 2)
+    .insertTile("TileCircle", undefined, false, "right")
+    .insertToast("Anywhere but down!", 2)
+    .insertTile("TileCircle", undefined, false, "down")
   for (let i = 0; i < 20; i++) {
     manager.insertTile({ TileSquare: 4, TileTapSquare: 1, TileCircle: 2 })
   }
--- src/levels/tuto-5.ts
+++ src/levels/tuto-5.ts
@@ -2,7 +2,7 @@ import { LevelManager } from "@/plugins/levelManager"
 
 export default function(manager: LevelManager) {
   manager
-    .insertToast("Rotate to align colors then tap", 2, 1)
+    .insertToast("Rotate, align, tap!", 3, 1)
     .insertTile("TileLosange", undefined, false)
     .insertTile("TileLosange", undefined, false)
   for (let i = 0; i < 40; i++) {
